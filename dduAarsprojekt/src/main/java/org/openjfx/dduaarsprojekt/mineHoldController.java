46f59cb5f26ead0e8fb7043b10199eb1d9b0c58c
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-11-17 13:59:40 +0100

    controllers

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/mineHoldController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/mineHoldController.java
@@ -3,18 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-/**
- *
- * @author Clara Maj
- */
 package org.openjfx.dduaarsprojekt;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-
-public class MainController {
-
+/**
+ *
+ * @author danie
+ */
+public class mineHoldController {
     @FXML
     private void mineHold() throws IOException {
         App.setRoot("mineHold");
