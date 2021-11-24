3575cf3a26ca7ece67cec045e1647d33160c73e3
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-11-24 11:30:19 +0100

    mindre fiks

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStaffController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStaffController.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 /**
  *
@@ -19,9 +20,17 @@ public class TestStaffController implements Initializable{
     @FXML
     TableView doneTest;
     TableView pendingTests;
+    TableColumn doneName;
+    TableColumn doneCorrect;
+    TableColumn doneParticipation;
+    TableColumn pendingName;
+    TableColumn pendingCorrect;
+    TableColumn pendingParticipation;
     
-    @Override
-    public void initialize(){
+    
+   @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         
     }
     
@@ -35,8 +44,5 @@ public class TestStaffController implements Initializable{
         App.setRoot("minePrøver");
     }
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
 }
