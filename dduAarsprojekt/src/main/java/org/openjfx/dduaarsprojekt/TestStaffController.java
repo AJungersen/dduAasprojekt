d0293f469607c4d3030bd0a788ca2bf42a554b06
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-08 23:46:35 +0100

    ting

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStaffController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStaffController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.openjfx.dduaarsprojekt.App;
 import org.openjfx.dduaarsprojekt.TestClasses.TaskSet;
+import org.openjfx.dduaarsprojekt.databaseRepository.TestDatabaseMethods;
 /**
  *
  * @author danie
@@ -35,7 +36,7 @@ public class TestStaffController implements Initializable{
    @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         ArrayList<TaskSet> tests = new ArrayList<>();
-        tests = UserDatabasemethods.getAllTaskSets();
+        tests = TestDatabaseMethods.getAllTaskSets();
     }
     
     @FXML
