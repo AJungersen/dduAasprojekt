5317f91062ef982b231e4a2835dc353313275ac0
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-10 13:04:54 +0100

    ting

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
@@ -8,7 +8,13 @@ package org.openjfx.dduaarsprojekt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.openjfx.dduaarsprojekt.TestClasses.TaskSet;
 
 /**
  * FXML Controller class
@@ -17,12 +23,24 @@ import javafx.fxml.Initializable;
  */
 public class TestStudentController implements Initializable {
    
-    /**
-     * Initializes the controller class.
-     */
+    ListView<String> OngoingTests;
+    TableView<TaskSet> done = new TableView();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        /*//hent alle prøve som eleven er igang med
+        ObservableList currentTests = getAllOngoingTests();
+        
+        ListView<String> OngoingTests = new ListView<>(currentTests);
+        
+        TableColumn<TaskSet,String> testName = new TableColumn<>("Prøve navn");
+        testName.setCellFactory(new PropertyValueFactory<>("name"));
+        
+        TableColumn<TaskSet,String> percent = new TableColumn<>("Rigtighedsprocent");
+        testName.setCellFactory(new PropertyValueFactory<>("name"));
+        
+        done.getColumns().add(testName);
+        done.getColumns().add(percent);*/
+    }   
 
 }
