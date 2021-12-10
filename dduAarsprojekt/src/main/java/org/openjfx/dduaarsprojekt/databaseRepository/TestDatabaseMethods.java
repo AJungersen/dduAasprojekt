e9e5ef6abdcd9288e69ebe0e284ad6ecf20c6bd2
stoftot <christoffer.gruberg@gmail.com>
2021-12-10 16:31:36 +0100

    #9
    temlate til et par founktioner

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/databaseRepository/TestDatabaseMethods.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/databaseRepository/TestDatabaseMethods.java
@@ -24,6 +24,78 @@ public class TestDatabaseMethods {
 
     private final String connectionString = "jdbc:sqlite:Database.db";
 
+    private ArrayList<TaskSet> getTaskSets(ResultSet rs, int _assignmentID) throws SQLException, Exception {
+        ArrayList <TaskSet> taskSets = new ArrayList<>();
+        
+        Connection conn = null;
+        Class.forName("org.sqlite.JDBC");
+
+        //Class.forName("org.mysql.JDBC.Driver");
+        //Skab forbindelse til databasen...
+        try {
+            conn = DriverManager.getConnection(connectionString);
+        } catch (SQLException e) {
+            //Skrive fejlhåndtering her
+            System.out.println("\n Database error (get all task set answers (connection): " + e.getMessage() + "\n");
+        }
+        
+        Statement stat = conn.createStatement();
+        
+        while (rs.next()) {
+            taskSets.add(new TaskSet(rs.getInt("taskSet_ID"), _assignmentID, rs.getInt("user_ID"), rs.getString("taskSetName"), rs.getString("description"), rs.getString("nameOfTheFiller"), (rs.getInt("handedIn") == 1 ? true : false), null));
+        }
+
+        //lav for loop til at hente alle task
+        for (int i = 0; i < taskSets.size(); i++) {
+            rs = stat.executeQuery("SELECT * FROM tasks WHERE taskSet_ID=('" + taskSets.get(i).getTaskSet_ID() + "')");
+
+            ArrayList<Task> tasks = new ArrayList<>();
+
+            //hent alle spørgsmål til task
+            while (rs.next()) {
+                Task task = new Task(rs.getInt("task_ID"), null, rs.getString("answer"), rs.getString("teacherComment"));
+
+                Question question = new Question(rs.getInt("question_ID"), rs.getString("question"));
+
+                ResultSet _rs;
+
+                switch (QuestionsType.valueOf(rs.getString("questionType"))) {
+                    case correctAnswerBasedQuestion:
+                        _rs = stat.executeQuery("SELECT * FROM CorrectAnswerBasedQuestion WHERE correctAnswerBasedQuestion_ID=('" + question.getQuestion_ID() + "')");
+
+                        task.setQuestion(new CorrectAnswerBasedQuestion(_rs.getString("coorectAnswer"), question.getQuestion_ID(), question.getQuestion()));
+                        break;
+                    case multipelChoiseQuestion:
+                        _rs = stat.executeQuery("SELECT * FROM MultipelChoiseAnswer WHERE MultipelChoiseQuestion_ID=('" + question.getQuestion_ID() + "')");
+
+                        MultipelChoiseQuestion multipelChoiseQuestion = new MultipelChoiseQuestion(null, question.getQuestion_ID(), question.getQuestion());
+
+                        ArrayList<MultipelChoiseAnswer> multipelChoiseAnswers = new ArrayList<>();
+
+                        while (_rs.next()) {
+                            multipelChoiseAnswers.add(new MultipelChoiseAnswer(_rs.getInt("multieplChoiseAnswer_ID"), _rs.getString("answer"), (_rs.getInt("correct") == 1 ? true : false)));
+                        }
+
+                        multipelChoiseQuestion.setAnswerOptions(multipelChoiseAnswers);
+
+                        task.setQuestion(multipelChoiseQuestion);
+                        break;
+                    case textAnswerBasedQuestion:
+
+                        task.setQuestion(new textAnswerBasedQuestion(question.getQuestion_ID(), question.getQuestion()));
+                        break;
+                }
+
+                tasks.add(task);
+            }
+            taskSets.get(i).setTasks(tasks);
+
+        }
+        return taskSets;
+    }
+    //--------------------------------------------------------------------
+    //------------------------------ public ------------------------------
+    //--------------------------------------------------------------------
     //----------------------------------------------
     //---------- get all task set answers ----------
     //----------------------------------------------
@@ -40,67 +112,17 @@ public class TestDatabaseMethods {
         } catch (SQLException e) {
             //Skrive fejlhåndtering her
             System.out.println("\n Database error (get all task set answers (connection): " + e.getMessage() + "\n");
-            System.out.println("\n Database error (check for matching user (connection): " + e.getMessage() + "\n");
         }
         try {
             Statement stat = conn.createStatement();
 
             //hent information til alle task setne
             ResultSet rs = stat.executeQuery("SELECT * FROM taskSets WHERE assignment_ID=('" + _assignmentID + "') AND WHERE NOT user_ID=('" + App.getLoggedInUser().getUser_ID() + "')");
-
-            while (rs.next()) {
-                taskSets.add(new TaskSet(rs.getInt("taskSet_ID"), _assignmentID, rs.getInt("user_ID"), rs.getString("taskSetName"), rs.getString("description"), rs.getString("nameOfTheFiller"), (rs.getInt("handedIn") == 1 ? true : false), null));
-            }
-
-            //lav for loop til at hente alle task
-            for (int i = 0; i < taskSets.size(); i++) {
-                rs = stat.executeQuery("SELECT * FROM tasks WHERE taskSet_ID=('" + taskSets.get(i).getTaskSet_ID() + "')");
-
-                ArrayList<Task> tasks = new ArrayList<>();
-
-                //hent alle spørgsmål til task
-                while (rs.next()) {
-                    Task task = new Task(rs.getInt("task_ID"), null, rs.getString("answer"), rs.getString("teacherComment"));
-
-                    Question question = new Question(rs.getInt("question_ID"), rs.getString("question"));
-
-                    ResultSet _rs;
-
-                    switch (QuestionsType.valueOf(rs.getString("questionType"))) {
-                        case correctAnswerBasedQuestion:
-                            _rs = stat.executeQuery("SELECT * FROM CorrectAnswerBasedQuestion WHERE correctAnswerBasedQuestion_ID=('" + question.getQuestion_ID() + "')");
-
-                            task.setQuestion(new CorrectAnswerBasedQuestion(_rs.getString("coorectAnswer"), question.getQuestion_ID(), question.getQuestion()));
-                            break;
-                        case multipelChoiseQuestion:
-                            _rs = stat.executeQuery("SELECT * FROM MultipelChoiseAnswer WHERE MultipelChoiseQuestion_ID=('" + question.getQuestion_ID() + "')");
-
-                            MultipelChoiseQuestion multipelChoiseQuestion = new MultipelChoiseQuestion(null, question.getQuestion_ID(), question.getQuestion());
-
-                            ArrayList<MultipelChoiseAnswer> multipelChoiseAnswers = new ArrayList<>();
-
-                            while (_rs.next()) {
-                                multipelChoiseAnswers.add(new MultipelChoiseAnswer(_rs.getInt("multieplChoiseAnswer_ID"), _rs.getString("answer"), (_rs.getInt("correct") == 1 ? true : false)));
-                            }
-
-                            multipelChoiseQuestion.setAnswerOptions(multipelChoiseAnswers);
-
-                            task.setQuestion(multipelChoiseQuestion);
-                            break;
-                        case textAnswerBasedQuestion:
-
-                            task.setQuestion(new textAnswerBasedQuestion(question.getQuestion_ID(), question.getQuestion()));
-                            break;
-                    }
-
-                    tasks.add(task);
-                }
-                taskSets.get(i).setTasks(tasks);
             
-            }
+            getTaskSets(rs, _assignmentID);
+            
             rs.close();
 
-            //lav for loop til at hente alle de forskellige spørgsmål til taskne
         } catch (SQLException e) {
             //Skrive fejlhåndtering her
             System.out.println("\n Database error (get all task set answers (get data): " + e.getMessage() + "\n");
@@ -162,7 +184,7 @@ public class TestDatabaseMethods {
             for (int i = 0; i < _taskSet.getTasks().size(); i++) {
 
                 int question_ID = 0;
-                
+
                 //insert questions from tasks
                 switch (_taskSet.getTasks().get(i).getQuestion().getType()) {
                     case multipelChoiseQuestion:
@@ -227,8 +249,8 @@ public class TestDatabaseMethods {
                 }
                 //insert task to questions
                 sql = "INSERT INTO tasks (taskSet_ID, question_ID, questionType, question) "
-                        + "VALUES ('" + taskSet_ID +"', '" + question_ID + "', "
-                        + "'" + _taskSet.getTasks().get(i).getQuestion().getType() +"', "
+                        + "VALUES ('" + taskSet_ID + "', '" + question_ID + "', "
+                        + "'" + _taskSet.getTasks().get(i).getQuestion().getType() + "', "
                         + "'" + _taskSet.getTasks().get(i).getQuestion().getQuestion() + "')";
 
                 try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -243,8 +265,34 @@ public class TestDatabaseMethods {
             System.out.println("\n Database error (create task set (insert): " + e.getMessage() + "\n");
         }
     }
+
+    //------------------------------------------------
+    //---------- get students task sets ----------
+    //------------------------------------------------
+    public ArrayList<Task> getStudentTasksSets () throws SQLException, Exception {
+        ArrayList<Task> studentTasksSets = new ArrayList<>();
+        
+        return studentTasksSets;
+    }
     
-    public static ArrayList<TaskSet> getAllTaskSets(){
+    //-----------------------------------------
+    //---------- get teams task sets ----------
+    //-----------------------------------------
+    public ArrayList<Task> getTeamsTaskSets () throws SQLException, Exception{
+        ArrayList<Task> teamTaskSets = new ArrayList<>();
+        
+        return teamTaskSets;
+    }
+    
+    //------------------------------------------------
+    //---------- get students teams ----------
+    //------------------------------------------------
+    
+    
+    
+    
+    
+     public static ArrayList<TaskSet> getAllTaskSets() {
         ArrayList alltasksets = new ArrayList();
         //placeholder
         // lav rigtig funktion senere
