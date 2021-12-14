2de3c65f282dc9bed811770ac27eb4f30ce4ef62
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-14 19:14:33 +0100

    Teams update

---
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/random/Teams.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openjfx.dduaarsprojekt.random;
+
+import java.util.ArrayList;
+import org.openjfx.dduaarsprojekt.TestClasses.TaskSet;
+
+/**
+ *
+ * @author danie
+ */
+public class Teams {
+    int Team_ID;
+    String teamName;
+    ArrayList<TaskSet> taskSets;
+    ArrayList<Student> students;
+    
+    Teams(int ID, String tName, ArrayList<TaskSet> tSets, ArrayList<Student> Studs){
+        
+    }
+    
+    Teams(String tName){
+        
+    }
+}
