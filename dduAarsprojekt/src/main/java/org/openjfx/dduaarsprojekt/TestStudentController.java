f9df606c59672cf75de31308a6df0262d182a842
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-15 15:21:28 +0100

    ting

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
@@ -7,7 +7,10 @@
 package org.openjfx.dduaarsprojekt;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +19,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.openjfx.dduaarsprojekt.TestClasses.TaskSet;
+import org.openjfx.dduaarsprojekt.random.Student;
 
 /**
  * FXML Controller class
@@ -25,25 +29,36 @@ import org.openjfx.dduaarsprojekt.TestClasses.TaskSet;
 public class TestStudentController implements Initializable {
     
     @FXML
-    ListView<String> OngoingTests;
+    ListView<String> teams;
     @FXML
     TableView<TaskSet> done = new TableView();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        /*//hent alle prøve som eleven er igang med
-        ObservableList currentTests = getAllOngoingTests();
-        
-        ListView<String> OngoingTests = new ListView<>(currentTests);
-        
-        TableColumn<TaskSet,String> testName = new TableColumn<>("Prøve navn");
-        testName.setCellFactory(new PropertyValueFactory<>("name"));
-        
-        TableColumn<TaskSet,String> percent = new TableColumn<>("Rigtighedsprocent");
-        testName.setCellFactory(new PropertyValueFactory<>("name"));
-        
-        done.getColumns().add(testName);
-        done.getColumns().add(percent);*/
+            String[] names = {};
+            ArrayList<TaskSet> nameListTask = new ArrayList();
+        try {
+            nameListTask = Student.getThisStudentTasks(App.getLoggedInUser().getUser_ID());
+        } catch (Exception ex) {
+            Logger.getLogger(TestStudentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            for(int i = 0; i < nameListTask.size(); i++){
+                
+            }
+            /*ObservableList currentTests = getAllOngoingTests();
+            
+            ListView<String> OngoingTests = new ListView<>(currentTests);
+            
+            TableColumn<TaskSet,String> testName = new TableColumn<>("Prøve navn");
+            testName.setCellFactory(new PropertyValueFactory<>("name"));
+            
+            TableColumn<TaskSet,String> percent = new TableColumn<>("Rigtighedsprocent");
+            testName.setCellFactory(new PropertyValueFactory<>("name"));
+            
+            done.getColumns().add(testName);
+            done.getColumns().add(percent);
+            Logger.getLogger(TestStudentController.class.getName()).log(Level.SEVERE, null, ex);*/
+        }
     }
     
     
