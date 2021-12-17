dad774ae5f3d41abef5a6f714d4f67fba0fc3fdb
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-17 21:10:04 +0100

    Lavet ting til elev view

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/TestStudentController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -34,11 +35,16 @@ public class TestStudentController implements Initializable {
     
     @FXML
     TableView<TaskSet> done = new TableView();
+    @FXML
+    ListView<String> teamsList = new ListView();
+    @FXML
+    ArrayList<TaskSet> selectedTeamTaskSets = new ArrayList();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
             ArrayList<String> taskSetNames = new ArrayList();
             ArrayList<TaskSet> nameListTask = new ArrayList();
+            String[] teamNamesString = {};
         try {
             nameListTask = Student.getThisStudentTasks(App.getLoggedInUser().getUser_ID());
         } catch (Exception ex) {
@@ -48,16 +54,23 @@ public class TestStudentController implements Initializable {
                 taskSetNames.add(nameListTask.get(i).getName());
             }
             //upload taskSetNames i den med alle taskSets
+            
             ArrayList<Team> teams = new ArrayList();
         try {
             teams = tdb.getStudentsTeams(App.getLoggedInUser().getUser_ID());
         } catch (Exception ex) {
             Logger.getLogger(TestStudentController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        for(int i = 0; i < teams.size(); i++){
+            teamNamesString[i] = teams.get(i).getTeamName();
         }
-    
-    @FXML
-    public void setTeamTasks(){
         
-    }
+        /*teamsList.getItems().addAll(teamNamesString);
+        teamsList.getSelectionModel().selectedItemIndex().addListener(new ChangeListener<String>(){
+            @Override
+            public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2){
+                selectedTeamTaskSets = teams.getSelectionModel().getSelectedItem().getTaskSet();
+            }
+        }*/
+        }
     }
