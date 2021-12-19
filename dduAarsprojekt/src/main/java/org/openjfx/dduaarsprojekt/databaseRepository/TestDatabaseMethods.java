4b361414906ce875bd7e2b5c6d528c7174fa16e4
stoftot <christoffer.gruberg@gmail.com>
2021-12-20 00:57:52 +0100

    git hub?

--- dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/databaseRepository/TestDatabaseMethods.java
+++ dduAarsprojekt/src/main/java/org/openjfx/dduaarsprojekt/databaseRepository/TestDatabaseMethods.java
@@ -1245,8 +1245,4 @@ public class TestDatabaseMethods {
         // lav rigtig funktion senere
         return alltasksets;
     }*/
-
-    public ArrayList<Student> getTeamsUnassignedStudents(int currentTeamID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
